680376f629fb977e1ee98d6f5fa3da084f8a81f3
zhihonghanniyasoft <134717724+zhihonghanniyasoft@users.noreply.github.com>
2026-09-22 10:48:07 -0400

    add data binding

--- coding/angular/day1/basicApp/src/app/components/my-component1/my-component1.ts
+++ coding/angular/day1/basicApp/src/app/components/my-component1/my-component1.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-component1',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './my-component1.html',
   styleUrl: './my-component1.scss',
 })
-export class MyComponent1 {}
+export class MyComponent1 {
+  // properties can be interpulated 
+  public myProperty: string = 'Hello, World!';
+  // private readonly object property cannot be interpulated directly in the template
+  private readonly objectProperty = { key: 'value', count: 90 };
+
+  public onClick(): void {
+    this.myProperty = 'New Value from click function';
+  }
+
+  public imgURL: string = 'https://avatars.githubusercontent.com/u/4586894?v=4&size=40';
+
+  public aInputValue: string = '';
+}
